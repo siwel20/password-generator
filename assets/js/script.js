2354eb11922590968caa3de6be4d0fc861709f15
siwel20 <plthrock@gmail.com>
2020-09-09 20:53:57 -0600

    added loop to help prepare the password, entered password length validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,20 @@ var characters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
 var numbers = "123456789";
 
 function generatePassword() {
-  
+  var preparePassword = true;
+  var passwordLength = 0;
+
+
+  //loop to require validate selections
+  while (preparePassword) {
+    while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      passwordLength = prompt("How long would you like your password to be? Your password length must be between 8 and 128.");
+      if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+        alert("Your password selection is not long enough. Please enter a number between 8 and 128");
+      }
+    }
+  }
+  return;
 }
 
 // Get references to the #generate element
